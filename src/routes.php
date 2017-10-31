f61b9be8915d00037224021e6a9471b81b0971c2
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 15:00:50 +1000

    +searchSiteAdmin Middleware

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,6 @@
 
 use Larrock\ComponentAdminSeo\AdminSeoController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SiteSearchAdmin']], function(){
     Route::resource('seo', AdminSeoController::class);
 });
