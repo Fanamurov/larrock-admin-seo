189800eb73677b4852eac21381884773748e2e02
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-09 15:15:40 +1000

    add AdminTraits

--- src/AdminSeoController.php
+++ src/AdminSeoController.php
@@ -2,30 +2,26 @@
 
 namespace Larrock\ComponentAdminSeo;
 
-use Alert;
 use Breadcrumbs;
 use Illuminate\Http\Request;
 
-use App\Http\Controllers\Controller;
-use Larrock\Core\AdminController;
-use Larrock\Core\Component;
 use Larrock\ComponentAdminSeo\Facades\LarrockSeo;
-use Redirect;
-use Validator;
-use View;
-use JsValidator;
+use Larrock\Core\Traits\AdminMethods;
+use Illuminate\Routing\Controller;
 
-class AdminSeoController extends AdminController
+class AdminSeoController extends Controller
 {
-	public function __construct()
-	{
+    use AdminMethods;
+
+    public function __construct()
+    {
         $this->config = LarrockSeo::shareConfig();
 
         \Config::set('breadcrumbs.view', 'larrock::admin.breadcrumb.breadcrumb');
         Breadcrumbs::register('admin.'. LarrockSeo::getName() .'.index', function($breadcrumbs){
             $breadcrumbs->push(LarrockSeo::getTitle(), '/admin/'. LarrockSeo::getName());
         });
-	}
+    }
 
     /**
      * Display a listing of the resource.
@@ -34,20 +30,20 @@ class AdminSeoController extends AdminController
      */
     public function index()
     {
-		$data['data'] = LarrockSeo::getModel()->orderBy('seo_type_connect')->paginate(30);
-		return view('larrock::admin.admin-builder.index', $data);
+        $data['data'] = LarrockSeo::getModel()->orderBy('seo_type_connect')->paginate(30);
+        return view('larrock::admin.admin-builder.index', $data);
     }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create(Request $request)
-	{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
         $test = Request::create('/admin/'. LarrockSeo::getName(), 'POST', [
             'seo_title' => 'Новый материал'
         ]);
         return $this->store($test);
-	}
+    }
 }
